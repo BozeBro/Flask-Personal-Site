f5147c18ccd2dea9d4b7e86a4559be36e4beaec3
BozeBro <ben.ozua@gmail.com>
2020-09-13 01:21:02 -0500

    Redirect home if logged in

--- website/routes.py
+++ website/routes.py
@@ -62,6 +62,8 @@ def project():
         )
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     # &$jLUM4B6Cg4WWf@
     form = LoginForm()
     if form.validate_on_submit():
