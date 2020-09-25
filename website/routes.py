65f78a9991d75f94f20171d4e7ccde66ed1197c6
BozeBro <ben.ozua@gmail.com>
2020-09-25 00:04:27 -0500

    keep password safe

--- website/routes.py
+++ website/routes.py
@@ -64,7 +64,6 @@ def project():
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
-    # &$jLUM4B6Cg4WWf@
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
